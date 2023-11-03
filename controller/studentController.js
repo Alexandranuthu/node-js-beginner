abfffaf3c27795a312be076cd57e1acf7d8f199e
Alexandranuthu <alexandranuthuu@gmail.com>
2023-11-03 09:31:20 +0300

    set up the student controller correctly

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,49 +1,49 @@
-const Student =require('../model/Studentsmodels')
+const Student = require('../model/Studentsmodels');
+const createError = require('http-errors'); // You need to require the 'http-errors' library
+const mongoose = require('mongoose');
 
-module.exports={
-    AddStudent: async (req, res) =>{
+module.exports = {
+    AddStudent: async (req, res) => {
         try {
-            const student = new Student(req.body); //use 'req.body' instead of 'res.body'
+            const student = new Student(req.body);
             const result = await student.save();
             res.send(result);
-        } catch (error){
+        } catch (error) {
             console.error(error.message);
         }
-        // res.send({type:"Post Request"});
-    }
-}
-
-getStudent: async(req, res, next)=>{
-    const id = req.params.id;
-    try{
-      const student = await Student.findById(id)
-      if(!student){
-        throw(createError(404, "student does not exist"))
-      }res.send(student)
-    }catch (error) {
-      console.log(error.message);
-      if(error instanceof mongoose.CastError){
-        next(createError(400, "invalid student id"));
-        return;
-      }
-      next(error);
-    }
-  }
-
-deleteStudent:async(req, res, next)=>{
-    const id = req.params.id
-    try {
-        const student = await Student.findByIdAndRemove(id)
-        if(!student){
-            throw(createError(404, "student does not exist"))
+    },
+    getStudent: async (req, res, next) => {
+        const id = req.params.id;
+        try {
+            const student = await Student.findById(id);
+            if (!student) {
+                throw createError(404, "Student does not exist");
+            }
+            res.send(student);
+        } catch (error) {
+            console.log(error.message);
+            if (error instanceof mongoose.CastError) {
+                next(createError(400, "Invalid student id"));
+                return;
+            }
+            next(error);
         }
-        res.send(student);
-    }catch(error){
-        console.log(error.message)
-        if(error instanceof mongoose.CastError){
-            next(createError(400, "invalid student id"));
-            return;
-          }
-          next(error);
+    },
+    deleteStudent: async (req, res, next) => {
+        const id = req.params.id;
+        try {
+            const student = await Student.findByIdAndRemove(id);
+            if (!student) {
+                throw createError(404, "Student does not exist");
+            }
+            res.send(student);
+        } catch (error) {
+            console.log(error.message);
+            if (error instanceof mongoose.CastError) {
+                next(createError(400, "Invalid student id"));
+                return;
+            }
+            next(error);
+        }
     }
-}
+};
