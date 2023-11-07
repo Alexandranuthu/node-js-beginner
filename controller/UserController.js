21bef8839e4c2de84dc736c7b4ebcc62f4383815
Alexandranuthu <alexandranuthuu@gmail.com>
2023-11-07 13:45:29 +0300

    added an access token

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,34 +1,62 @@
 const User = require('../model/Usermodels');
 const createError = require('http-errors'); // You need to require the 'http-errors' library
 const mongoose = require('mongoose');
+const authSchema = require("../auth/auth_schema").authSchema;
+const createHttpError = require('http-errors');
+const { signAccessToken } = require('../helpers/JWThelper');
 
 module.exports = {
-    Adduser: async (req, res) => {
+
+    //add user works
+    Adduser: async (req, res, next) => {
+     try {
+         // Extract user registration data from the request body
+         const { email, password } = req.body;
+            const result = await authSchema.validateAsync(req.body);
+
+            const Exists = await User.findOne({email: email})
+
+            if (Exists) throw createHttpError.Conflict(`${email} is already been registered`)
+            const user = new User(result)
+
+         const savedUser = await user.save()
+         const accessToken = await signAccessToken(savedUser.id)
+         res.send({accessToken});
+
+     } catch (error) {
+            if(error.isJoi === true)error.status = 422
+            next(error)
+     }
+ },
+
+
+    getUser:  async (req, res) => {
         try {
-            const user = new User(req.body);
-            const result = await user.save();
+            const result = await User.find({});
+            res.send(result); // Sending the result as the response
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    },
+
+    updateUser:async(req, res, next) => {
+        try{
+            const id = req.params.id;
+            const update = req.body;
+            const options = {new: true}
+            const result = await User.findByIdAndUpdate(id, update, options)
+            
             res.send(result);
-        } catch (error) {
-            console.error(error.message);
+        }catch(error){
+            console.log(error.message)
         }
     },
-    getUser: async (req, res, next) => {
-        const id = req.params.id;
-        try {
-            const user = await User.findById(id);
-            if (!user) {
-                throw createError(404, "User does not exist");
-            }
-            res.send(user);
-        } catch (error) {
-            console.log(error.message);
-            if (error instanceof mongoose.CastError) {
-                next(createError(400, "Invalid user id"));
-                return;
-            }
-            next(error);
-        }
-    },
+ 
+    
+    
+
+    //DELETE USER WORKS
     deleteUser: async (req, res, next) => {
         const id = req.params.id;
         try {
--- routes/Userroute.js
+++ routes/Userroute.js
@@ -1,100 +1,17 @@
 const express = require("express");
 const route = express.Router();
-const mongoose = require("mongoose"); // Import Mongoose for MongoDB connection
-const User = require("../model/Usermodels");
-const createHttpError = require('http-errors');
-// Connect to your MongoDB database
+const UserController = require("../controller/UserController");
 
 
 // Get a list of registered users from the database
-route.get('/register', async (req, res) => {
-    try {
-        const result = await user.find({});
-        res.send(result); // Sending the result as the response
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-//POST
-// route.post('/register', async (req, res, next) => {
-//     try {
-//         // Extract user registration data from the request body
-//         const { email, password } = req.body;
+route.get('/register', UserController.getUser);
 
-//         // Create a new user document in MongoDB
-//         const newUser = new user({
-//             email,
-//             password,
-//         });
-
-//         // Save the new user document to the database
-//         const result = await newUser.save();
-
-//         res.status(201).json(result); // Send the newly created user as a response
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
+route.patch('/register/:id', UserController.updateUser);
 
 const authSchema = require("../auth/auth_schema").authSchema;
 //POST
- route.post('/register', async (req, res, next) => {
-     try {
-         // Extract user registration data from the request body
-         const { email, password } = req.body;
-            const result = await authSchema.validateAsync(req.body);
-
-            const Exists = await User.findOne({email: email})
-
-            if (Exists) throw createHttpError.Conflict(`${email} is already been registered`)
-            const user = new User(result)
-
-         const savedUser = await user.save()
-
-     } catch (error) {
-            if(error.isJoi === true)error.status = 422
-            next(error)
-     }
- });
-
-// const authSchema = require("../auth/auth_schema").authSchema; // Import the authSchema object
-
-// route.post('/register', async (req, res, next) => {
-//     try {
-//         // Extract user registration data from the request body
-//         const { email, password } = req.body;
-
-//         // Use the authSchema for validation
-//         const { error, value } = authSchema.validate({
-//             email: email,
-//             password: password
-//         });
-
-//         if (error) {
-//             // Joi validation failed
-//             throw createHttpError(422, error.details[0].message);
-//         }
-
-//         const Exists = await User.findOne({ email: email });
-
-//         if (Exists) {
-//             throw createHttpError.Conflict(`${email} is already registered`);
-//         }
-
-//         const user = new User(value); // Use the validated 'value' to create the user
-
-//         const savedUser = await user.save();
-
-//         res.status(201).json(savedUser); // Send the newly created user as a response
-//     } catch (error) {
-//         if (error.isJoi === true) {
-//             error.status = 422;
-//         }
-//         next(error);
-//     }
-// });
+route.post('/register', UserController.Adduser);
+route.delete('/register/:id', UserController.deleteUser);
 
 
 
